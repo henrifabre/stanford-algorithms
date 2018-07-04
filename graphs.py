4353d915fec14d97e2d12d04036f7e1232071eab
Henri Fabre <henrifabre@protonmail.com>
2018-07-05 01:25:31 +0530

    Strongly connected component implementation #pa4

--- graphs.py
+++ graphs.py
@@ -1,29 +1,85 @@
 from collections import deque
+import copy
+# import line_profiler
 
 
-def graphsearch_bfs(graph, startNode, bfs_explored_nodeList=[]):
+def graphsearch_bfs(graph, startNode, explored_nodeList=[]):
         node_queue = deque()    # Invariant - does not contain explored nodes
         node_queue.append(startNode)
-        bfs_explored_nodeList.append(startNode)
+        explored_nodeList.append(startNode)
         while node_queue:
                 current_node = node_queue.popleft()
                 for node in graph[current_node]:
-                        if node not in bfs_explored_nodeList:
+                        if node not in explored_nodeList:
                                 node_queue.append(node)
-                                bfs_explored_nodeList.append(node)
+                                explored_nodeList.append(node)
 
-        return bfs_explored_nodeList
+        return explored_nodeList
 
 
-def graphsearch_dfs(graph, startNode, dfs_seen_nodeList=[]):
-        node_stack = deque()
-        node_stack.append(startNode)
-        dfs_seen_nodeList.append(startNode)
-        for node in graph[startNode]:
-                if node not in dfs_seen_nodeList:
-                        graphsearch_dfs(graph, node, dfs_seen_nodeList)
+# @profile
+def graphsearch_dfs(graph, startNode, seen_nodeList=set(), explored_nodeStack=[]):
+        nodeStack = []
+        if startNode not in seen_nodeList:
+                seen_nodeList.add(startNode)
+                nodeStack.append(startNode)
+        while nodeStack:
+                currentNode = nodeStack[-1]
+                exploredFlag = True
+                for node in graph[currentNode]:
+                        if node not in seen_nodeList:
+                                nodeStack.append(node)
+                                seen_nodeList.add(node)
+                                exploredFlag = False
+                if exploredFlag:
+                        explored_nodeStack.append(nodeStack.pop())
 
-        return dfs_seen_nodeList
+        return seen_nodeList, explored_nodeStack
+
+
+# DFS search using recursive method
+# def graphsearch_dfs(graph, startNode, seen_nodeList=deque(), explored_nodeStack=deque()):
+#         if startNode not in seen_nodeList:
+#                 seen_nodeList.append(startNode)
+#         for node in graph[startNode]:
+#                 if node not in seen_nodeList:
+#                         graphsearch_dfs(graph, node, seen_nodeList, explored_nodeStack)
+#         explored_nodeStack.append(startNode)
+#         return seen_nodeList, explored_nodeStack
+
+
+# Assumption: graph is a dict with keys as ardered integers starting from 1
+#             because we start exploring the graph from vertex 1
+# @profile
+def graph_findSCC(graph, graphR, seen_nodeList=set(), explored_nodeStack=[]):
+        # 1st pass
+        for i in range(1, max(graph)+1):
+                if i not in seen_nodeList:
+                        seen_nodeList, explored_nodeStack = graphsearch_dfs(
+                                graph, i, seen_nodeList=seen_nodeList)
+
+        # 2nd pass
+
+        seen_nodeList.clear()
+        scc_list = []
+        unexplored_nodeStack = copy.deepcopy(explored_nodeStack)
+        unexplored_nodeSet = set(unexplored_nodeStack)
+        print("First pass done")
+        while unexplored_nodeStack:
+                scc_list.append([unexplored_nodeStack.pop()])
+                unexplored_nodeSet.remove(scc_list[-1][0])
+                seen_nodeList, __t = graphsearch_dfs(graphR,
+                                                     startNode=scc_list[-1][0],
+                                                     seen_nodeList=seen_nodeList)
+                newlySeenNodeSet = seen_nodeList & unexplored_nodeSet
+                for el in newlySeenNodeSet:
+                        scc_list[-1].append(el)
+                        # unexplored_nodeStack.remove(el)
+                        unexplored_nodeSet.remove(el)
+                while unexplored_nodeStack and unexplored_nodeStack[-1] in seen_nodeList:
+                        unexplored_nodeStack.pop()
+
+        return seen_nodeList, scc_list
 
 
 def find_path(graph, start, end, path=[]):
--- test_graphs.py
+++ test_graphs.py
@@ -2,19 +2,54 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import graphs
 
-inputGrf = {'S': ['B', 'A'],
-                'E': ['D', 'C'],
-                'B': ['D', 'C', 'S'],
-                'D': ['C', 'B', 'E'],
-                'A': ['C', 'S'],
-                'C': ['A', 'B', 'D', 'E']}
+# inputGrf = {1: ['B', 2],
+#                 'E': ['D', 'C'],
+#                 'B': ['D', 'C', 1],
+#                 'D': ['C', 'B', 'E'],
+#                 2: ['C', 1],
+#                 'C': [2, 'B', 'D', 'E']}
 
-nxDrawGraph = True
+inputGrf = {}
+inputGrfR = {}
+with open("SCC.txt") as f:
+        for line in f:
+                edge = line.split()
+                startNode = int(edge[0])
+                endNode = int(edge[1])
+                if startNode in inputGrf:
+                        inputGrf[startNode].append(endNode)
+                else:
+                        inputGrf[startNode] = [endNode]
+                if endNode not in inputGrf:
+                        inputGrf[endNode] = []
+
+                if endNode in inputGrfR:
+                        inputGrfR[endNode].append(startNode)
+                else:
+                        inputGrfR[endNode] = [startNode]
+                if startNode not in inputGrfR:
+                        inputGrfR[startNode] = []
+
+
+# print(inputGrf)
+nxDrawGraph = False
+
+if len(inputGrf) > 50:
+        nxDrawGraph = False
+
+seen_order, explored_order = graphs.graph_findSCC(inputGrf, inputGrfR)
+# print("Seen Order:  ")
+# print(seen_order)
+# print("Explored Order:  ")
+# print(explored_order)
+scc_size = []
+for el in explored_order:
+        scc_size.append(len(el))
+scc_size.sort(reverse=True)
+print(scc_size[:5])
+print(len(scc_size))
+
 if nxDrawGraph is True:
-        nxGrf = nx.Graph(inputGrf)
-        nx.draw(nxGrf, with_labels=True)
+        nxGrf = nx.DiGraph(inputGrfR)
+        nx.draw(nxGrf, pos=nx.shell_layout(inputGrf), with_labels=True)
         plt.show()
-
-bfs_order = graphs.graphsearch_dfs(inputGrf, 'S')
-print("BFS Order:  ")
-print(bfs_order)
