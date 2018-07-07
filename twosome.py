47f5e0bf8a231fc874d4647df462fed022e1c832
Henri Fabre <henrifabre@protonmail.com>
2018-07-07 18:03:17 +0530

    Changed checking range to [-10000, 10000]

--- twosome.py
+++ twosome.py
@@ -1,15 +1,23 @@
+import line_profiler
 
-twosomeArr = []
+
+twosomeSet = set()
 with open("2sum_data.txt") as f:
         for line in f:
-                twosomeArr.append(int(line))
-twosomeSet = set(twosomeArr)
+                twosomeSet.add(int(line))
+print(len(twosomeSet))
 
-twosomeCount = 0
-for i in range(-10, 101):
-        for el in twosomeArr:
-                if (i - el) in twosomeSet:
-                        print(i, el)
-                        twosomeCount = twosomeCount + 1
-                        break
-print(twosomeCount)
+
+# @profile
+def twosome_count(twosomeSet):
+        twosomeCount = 0
+        for i in range(-10000, 10001):
+                for el in twosomeSet:
+                        if (i - el) in twosomeSet:
+                                print(i, el)
+                                twosomeCount = twosomeCount + 1
+                                break
+        print(twosomeCount)
+
+
+twosome_count(twosomeSet)
